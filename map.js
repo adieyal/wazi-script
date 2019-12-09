5616aac457f6324ee097ec8b3bc120afd264bf55
Adi Eyal <adi@code4sa.org>
2019-12-09 08:53:31 +0200

    Update map.js

--- map.js
+++ map.js
@@ -1,5 +1,5 @@
 url = "https://mapit.openup.org.za/areas/MDB-levels:CY-ZA%7CPR.geojson?generation=1&simplify_tolerance=0.005"
-var map = L.map('main-map').setView([-32.8685099,20.7031068], 7);
+var map = L.map('main-map', { zoomControl: false}).setView([-32.8685099,20.7031068], 7);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 L.control.zoom({position: 'topright'}).addTo(map);
 $.getJSON(url, function(data) {
